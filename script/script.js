0f9d32b7416ed9450fcfa405dea0d13199723a15
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-08-05 00:23:02 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector('.button')
+const input = document.querySelector('#inputs')
+const items = document.querySelector('#items')
+btn.addEventListener("click",()=>{
+    if(input.value == ''){
+        alert('hello world')
+    }else{
+        const li = document.createElement('li')
+        li.innerText = input.value
+        const x = document.createElement('span')
+        x.innerHTML = '\u00d7'
+        li.appendChild(x)
+        const ul = document.createElement('ul')
+        ul.appendChild(li)
+        items.appendChild(ul)
+        input.value = ''
+        Store()
+    }
+})
+
+items.addEventListener('click',(tag)=>{
+    if(tag.target.tagName === 'LI'){
+        tag.target.classList.toggle('click')
+        Store()
+    }else if(tag.target.tagName === 'SPAN'){
+        tag.target.parentElement.remove()
+        Store()
+    }
+})
+
+function Store(){
+    localStorage.setItem("data",items.innerHTML)
+}
+function display(){
+    items.innerHTML = localStorage.getItem("data")
+}
+display()
